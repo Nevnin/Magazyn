8186ef810b1389eec4417aee0de10246813d8d6e
Mac <Mac@Mac-Komputer>
2017-03-08 11:56:20 +0100

    wersja 1.3
    -dodane polaczenie

--- src/Polaczenie.java
+++ src/Polaczenie.java
@@ -1,4 +1,29 @@
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class Polaczenie {
+import javax.swing.JFrame;
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+
+public class Polaczenie{
+
+	private final static String DBURL="jdbc:mysql://localhost:3306/magazyn";
+	private final static String DBUSER = "root";
+	private final static String DBPASS = "";
+	private final static String DBDRIVER = "com.mysql.jdbc.Driver";
+	
+	private Connection connection;
+	private Statement statement;
+	
+	public Polaczenie() throws SQLException{
+		connection = (Connection) DriverManager.getConnection(DBURL, DBUSER, DBPASS);
+		statement = (Statement) connection.createStatement();
+	}
+	
+	public void sqlSelect(String query) throws SQLException{
+		ResultSet rs = statement.executeQuery(query);
+	}
+	
 }
