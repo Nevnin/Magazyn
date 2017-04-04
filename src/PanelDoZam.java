5b1e0e3533a981cdc0ab55f3594cd0863bdc73a7
DamianB02 <brzecki.damian@gmail.com>
2017-04-04 22:31:17 +0200

    dodanie zamowienia ale jeszcze nie skonczone do konca xd

---
+++ src/PanelDoZam.java
@@ -0,0 +1,54 @@
+import java.awt.Button;
+import java.awt.Dimension;
+import java.awt.Label;
+import java.awt.TextField;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class PanelDoZam extends JPanel {
+	
+		JTextField tfTerminRealizacji,tfDataRealizacji;
+	    JLabel lTerminRealizacji,lDataRealizacji;
+	    JButton zamow,DodajDoZamowienia;
+	    	    public PanelDoZam(int numer) {
+	        setSize(new Dimension(400, 100));
+	        
+	    	setLayout(null);
+	    	
+	    	
+	    	if(numer == 0){
+	    	JLabel lblTerminRealizacji = new JLabel("Termin Realizacji");
+	    	lblTerminRealizacji.setBounds(10, 11, 88, 14);
+	    	add(lblTerminRealizacji);
+	    	
+	    	tfTerminRealizacji = new JTextField();
+	    	tfTerminRealizacji.setBounds(108, 8, 86, 20);
+	    	tfTerminRealizacji.setToolTipText("Podaj date w formacie: yyyy-mm-dd");
+	    	add(tfTerminRealizacji);
+	    	tfTerminRealizacji.setColumns(10);
+	    	
+	    	JLabel lblDataRealizacji = new JLabel("Data Realizacji");
+	    	lblDataRealizacji.setBounds(10, 36, 88, 14);
+	    	add(lblDataRealizacji);
+	    	
+	    	tfDataRealizacji = new JTextField();
+	    	tfDataRealizacji.setBounds(108, 33, 86, 20);
+	    	tfDataRealizacji.setToolTipText("Podaj date w formacie: yyyy-mm-dd");
+	    	add(tfDataRealizacji);
+	    	tfDataRealizacji.setColumns(10);
+	    	
+	    	zamow = new JButton("Zamow");
+	    	zamow.setBounds(301, 61, 89, 23);
+	    	add(zamow);
+	    	}
+	    	else
+	    	{
+	    		DodajDoZamowienia = new JButton("Dodaj do zamowienia");
+		    	DodajDoZamowienia.setBounds(257, 66, 133, 23);
+		    	add(DodajDoZamowienia);
+	    	}
+	    }   
+}
--- src/Zamowienie.java
+++ src/Zamowienie.java
@@ -1,26 +1,72 @@
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
+import javax.swing.JTextField;
 import javax.swing.ListModel;
 import javax.swing.ListSelectionModel;
+import javax.swing.border.Border;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 import java.awt.Dimension;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Properties;
 
+import javax.swing.JButton;
 import javax.swing.JDesktopPane;
+import javax.swing.JLabel;
 import javax.swing.JList;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Label;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Zamowienie extends JPanel implements ListSelectionListener {
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
+
+import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.scene.layout.VBox;
+
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.border.LineBorder;
+
+public class Zamowienie extends JPanel implements ActionListener {
+	 String serverName = "localhost";
+	    String mydatabase = "magazyn";
+	    String url = "jdbc:mysql://" + serverName + "/" + mydatabase; 
+	    String username = "root";
+	    String password = "";
 	Polaczenie poloczenie;
 	public JList list_1,list_2;
+	private JList list_1_2;
 	private JList list_1_1;
 	String[] tablica;
 	String[] tablica1;
+	JButton zamow ;
 	JSplitPane splitPane;
+	PanelDoZam p;
 	public Zamowienie() {
-		//setPreferredSize(new Dimension(400,400));
+		setBorder(new LineBorder(new Color(0, 0, 0)));
+		setPreferredSize(new Dimension(400,300));
+		GridLayout g= new GridLayout(2,1);
+		setLayout(g);
 		try {
 			poloczenie = new Polaczenie();
 		} catch (SQLException e) {
@@ -46,68 +92,161 @@ public class Zamowienie extends JPanel implements ListSelectionListener {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		 splitPane = new JSplitPane();
+//		SimpleDateFormat dt= new SimpleDateFormat("yyyy-MM-dd"); 
+//		 Date data = new Date(); 
+//		System.out.println(dt.format(data));
+		splitPane = new JSplitPane();
+		 splitPane.setBounds(85, 5, 207, 202);
 		// splitPane.setPreferredSize(new Dimension(400, 400));
 		// splitPane.setMinimumSize(new Dimension(400, 200));
-		add(splitPane);
+		add(splitPane,g);
 		
-		 list_1 = new JList();
 		 list_2 = new JList(); 
+		 list_2.setPreferredSize(new Dimension(200,200));
 		 list_2.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		 
 		 splitPane.setRightComponent(list_2);
-		 list_1.setAlignmentX(CENTER_ALIGNMENT);
-		list_1 = new JList(tablica);
+		list_1_2 = new JList(tablica);
+		 list_1_2.setSize(new Dimension(200,200));
+		 
 	//	list_1.setPreferredSize(new Dimension(200, 10));
-		list_1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		list_1.addListSelectionListener(this);
-		splitPane.setLeftComponent(list_1);
-		
-		
-		
-		
-		
-		
-		
-	}
-	
-	@Override
-	public void valueChanged(ListSelectionEvent e) {
-		
-		if(e.getValueIsAdjusting()==true)
-		{
-			String query = "SELECT dostawca.NazwaSkrocona FROM `towar` INNER JOIN dostawcatowar ON dostawcatowar.IdTowar=towar.IdTowar INNER JOIN dostawca ON dostawca.IdDostawca= dostawcatowar.IdDostawca WHERE towar.NazwaTowaru ='"+list_1.getSelectedValue()+"'GROUP BY dostawca.NazwaSkrocona";
-			
-			try {
-				ResultSet rs = poloczenie.sqlSelect(query);
-				rs.last();
-				int rozmiar = rs.getRow();
-				rs.beforeFirst();
-				tablica1 = new String[rozmiar];
-				int i =0;
-				while(rs.next())
+		list_1_2.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list_1_2.addListSelectionListener(new ListSelectionListener(){
+
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				if(e.getValueIsAdjusting()==true)
 				{
+					String query = "SELECT dostawca.NazwaSkrocona FROM `towar` INNER JOIN dostawcatowar ON dostawcatowar.IdTowar=towar.IdTowar INNER JOIN dostawca ON dostawca.IdDostawca= dostawcatowar.IdDostawca WHERE towar.NazwaTowaru ='"+list_1_2.getSelectedValue()+"'GROUP BY dostawca.NazwaSkrocona";
 					
-					tablica1[i]=rs.getString(1);
-					i++;
-					
-				}
-				list_2 = new JList(tablica1);
-				list_2.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-				list_2.addListSelectionListener(this);
-				splitPane.setRightComponent(list_2);
-				 list_2.setAlignmentX(CENTER_ALIGNMENT);
-				validate();
+					try {
+						ResultSet rs = poloczenie.sqlSelect(query);
+						rs.last();
+						int rozmiar = rs.getRow();
+						rs.beforeFirst();
+						tablica1 = new String[rozmiar];
+						int i =0;
+						while(rs.next())
+						{
+							
+							tablica1[i]=rs.getString(1);
+							i++;
+							
+						}
+						list_2 = new JList(tablica1);
+						list_2.setPreferredSize(new Dimension(200,200));
+						list_2.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+						list_2.addListSelectionListener(new ListSelectionListener(){
+
+							@Override
+							public void valueChanged(ListSelectionEvent e) {
+								if(e.getValueIsAdjusting()==true)
+								{
+									p = new PanelDoZam(0);
+									add(p,g);
+									validate();
+									repaint();
+									ustawNasluchZdarzen();
+								}
+								
+							}});
+						
+						splitPane.setRightComponent(list_2);
+						 list_2.setAlignmentX(CENTER_ALIGNMENT);
+						validate();
+						repaint();
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
 				
-				repaint();
-			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
+				}}});
+
+		splitPane.setLeftComponent(list_1_2);
+			
+	}
+	private void ustawNasluchZdarzen() 
+	{
+		p.zamow.addActionListener(this);
+		p.DodajDoZamowienia.addActionListener(this);
+	}
+	public void Zamowienie(Date TerminRealizacji, Date DataRealizacji,float KosztZamowienia , int IdDostawcy , String NumerZamowienia, String SposobDostawy , float KosztDostawy) throws SQLException
+	{
+		Connection connection = DriverManager.getConnection(url, username, password);
+		String query = "INSERT INTO zamowienie "
+				+ "(TerminReallizacji,DataRealizacji,KosztZamowienia,IdDostawcy,DataWystawienia,NumerZamowienia,SposobDostawy,KosztDostawy,WartoscTowarow)"
+			    + " values (?, ?, ?, ?, ?, ?,?,?,?)";
+		PreparedStatement preparedStmt = connection.prepareStatement(query,Statement.RETURN_GENERATED_KEYS);
+		preparedStmt.setDate (1,(java.sql.Date) TerminRealizacji);
+		preparedStmt.setDate (2,(java.sql.Date) DataRealizacji);
+		preparedStmt.setFloat (3,KosztZamowienia);
+		preparedStmt.setInt (4,IdDostawcy);
+		Date dzis = new Date();
+		preparedStmt.setDate (5,(java.sql.Date) dzis);
+		preparedStmt.setString (6,NumerZamowienia);
+		preparedStmt.setString (7,SposobDostawy);
+		preparedStmt.setFloat (8,KosztDostawy);
+		float WartoscDostawy = KosztDostawy+KosztZamowienia;
+		preparedStmt.setFloat (9,WartoscDostawy);
+		preparedStmt.execute();
+		connection.close();
+	}
+	public void dodanieTowaruDoZamowienia(int idZam, int idTow,int ilosc, double cena) throws SQLException
+	{
+		Connection connection = DriverManager.getConnection(url, username, password);
+		String query = "INSERT INTO zamowienietowar "
+				+ "(LP,IdTowar,Cena,Ilosc,WartoscNetto,IdZamowienie)"
+			    + " values (?, ?, ?, ?, ?, ?)";
+		int LP = LP(idZam)+1;
+		PreparedStatement preparedStmt = connection.prepareStatement(query,Statement.RETURN_GENERATED_KEYS);
+		preparedStmt.setInt (1,LP);
+		preparedStmt.setInt (2, idTow);
+		preparedStmt.setDouble (3, cena);
+		preparedStmt.setInt (4, ilosc);
+		double wartosc= cena * ilosc;
+		preparedStmt.setDouble (5,wartosc);
+		preparedStmt.setInt (6,idZam);
+		preparedStmt.execute();
+		String query2= "UPDATE zamowienie set KosztZamowienia =KosztZamowienia + ? WHERE IdZamowienie=?";
+		PreparedStatement preparedStmt2 = connection.prepareStatement(query2);
+		
+		preparedStmt2.setDouble(1,wartosc);
+		preparedStmt2.setInt(2,idZam);
+		preparedStmt.execute();
+		connection.close();
+	}
+	public int LP(int idZam)
+	{
+		String query = "Select LP from zamowienietowar INNER JOIN zamowienie WHERE IdZamowienie="+idZam; 
+		ResultSet rs;
+		int LP=0;
+		try {
+			rs = poloczenie.sqlSelect(query);
+			
+			while(rs.next())
+			{
+				if(LP<rs.getInt(1))
+				{
+					LP=rs.getInt(1);
+				}
 			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
+		return LP;
 	}
-	
-	
-
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object z= e.getSource();
+		if(z==p.zamow)
+		{
+			
+		}
+		
+	} 
 }
+
+
+
