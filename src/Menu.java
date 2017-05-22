c9938a271aa087d910a5194efb1f84ea9ac714d3
Maciek <Maciek@192.168.1.2>
2017-05-22 21:37:46 +0200

    nowe menu

--- src/Menu.java
+++ src/Menu.java
@@ -4,8 +4,8 @@ import javax.swing.JMenuItem;
 
 public class Menu extends JMenuBar {
     JMenuBar menuBar;
-    JMenu plik , pomoc ,towar , dostawcy ,zamowienie;
-    JMenuItem zaklkartdostawcy, wykazdostawcow, szukanietowarow, historiazamowien, zamowienietowaru, stanmagazynowy , reklamacja,towarydostawcy;  
+    JMenu plik , pomoc ,towar , dostawcy ,zamowienie, wyszukiwanie;
+    JMenuItem zaklkartdostawcy, wykazdostawcow, szukanietowarow, historiazamowien, zamowienietowaru, stanmagazynowy , reklamacja,towarydostawcy, danyOkres, danyTowar, wgWartosci, wgKategorii;  
     
     public Menu() {
         menuBar =  new JMenuBar();
@@ -14,11 +14,15 @@ public class Menu extends JMenuBar {
         towar = new JMenu("Towar");
         dostawcy = new JMenu("Dostawcy");
         zamowienie = new JMenu("Zamowienie");
+        wyszukiwanie = new JMenu("Wyszukiwanie");
+             
         menuBar.add(plik);
         menuBar.add(zamowienie);
         menuBar.add(towar);
         menuBar.add(dostawcy);
         menuBar.add(zamowienie);
+        
+        
         zaklkartdostawcy = new JMenuItem("Zak³adanie karty dostawcy");
         wykazdostawcow = new JMenuItem("Wykaz dostawców");
         szukanietowarow = new JMenuItem("Szukanie towarów");
@@ -27,10 +31,22 @@ public class Menu extends JMenuBar {
         stanmagazynowy = new JMenuItem("Stan magazynowy");
         reklamacja = new JMenuItem("Reklamacja towaru");
         towarydostawcy = new JMenuItem("Wyszukiwanie");
+        danyTowar = new JMenuItem("Dany towar");
+        danyOkres = new JMenuItem("Dany okres");
+        wgWartosci = new JMenuItem("Wed³ug wartoœci");
+        wgKategorii = new JMenuItem("Wed³ug kategorii");
+        
         towar.add(szukanietowarow);
         towar.add(towarydostawcy);
         towar.add(stanmagazynowy);
         towar.add(reklamacja);
+        towar.add(wyszukiwanie);
+        
+        wyszukiwanie.add(danyTowar);
+        wyszukiwanie.add(danyOkres);
+        wyszukiwanie.add(wgWartosci);
+        wyszukiwanie.add(wgKategorii);
+        
         dostawcy.add(zaklkartdostawcy);
         dostawcy.add(wykazdostawcow);
         zamowienie.add(zamowienietowaru);
